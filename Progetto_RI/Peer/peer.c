748eefd3335c235ebea4a648255f872dc93ef675
Frusco <frusco22@gmail.com>
2021-04-12 19:35:58 +0200

    started peer, finished server

---
+++ Progetto_RI/Peer/peer.c
@@ -0,0 +1,318 @@
+#include "../consts/const.h"
+#include "../libs/my_sockets.h"
+#include "../libs/my_parser.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <pthread.h>
+
+/*
+### Neighbors struct ############################
+*/
+int my_id;
+fd_set master;
+fd_set reading;
+pthread_mutex_t fd_mutex;
+struct neighbour{
+    int id;
+    struct sockaddr_in addr;
+    int socket;
+    struct neighbour* next;
+};
+pthread_mutex_t neighbors_list_mutex;
+int neighbors_number;
+struct neighbour* neighbors_list;
+
+
+struct neighbour* neighbour_init_and_connect(int id,struct in_addr addr,int port){
+    struct neighbour *n = malloc(sizeof(struct neighbour));
+    n->id = id;
+    n->next = NULL;
+    n->socket = socket(AF_INET,SOCK_STREAM,0);
+    memset(&n->addr,0,sizeof(n->addr)); //Pulizia
+    n->addr.sin_family = AF_INET; //Tipo di socket
+    n->addr.sin_port = htons(port);//Porta
+    n->addr.sin_addr = addr;
+    if(connect(n->socket,(struct sockaddr*)&n->addr,sizeof(n->addr))==0){
+        return n;
+    }else{
+        perror("Errore nell'apertura della neighbour socket");
+        return NULL;
+    }
+}
+
+struct neighbour* neighbour_init(int id,int socket,struct in_addr addr,int port){
+    struct neighbour *n = malloc(sizeof(struct neighbour));
+    n->id = id;
+    n->next = NULL;
+    n->socket = socket;
+    memset(&n->addr,0,sizeof(n->addr)); //Pulizia
+    n->addr.sin_family = AF_INET; //Tipo di socket
+    n->addr.sin_port = htons(port);//Porta
+    n->addr.sin_addr = addr;
+    return n;
+}
+
+// inserimento in coda
+void neighbors_list_add(struct neighbour* n){
+    struct neighbour*cur,*prev;
+    pthread_mutex_lock(&neighbors_list_mutex);
+    cur = neighbors_list;
+    while(cur){
+        prev = cur;
+        cur = cur->next;
+    }
+    prev->next = n;
+    neighbors_number++;
+    pthread_mutex_unlock(&neighbors_list_mutex);
+}
+// rimozione per id
+int neighbors_list_remove(int id){
+    pthread_mutex_lock(&neighbors_list_mutex);
+    struct neighbour*cur,*prev=NULL;
+    int ret = 0;
+    cur = neighbors_list;
+    while(cur){
+        if(cur->id == id){
+            break;
+        }
+        prev = cur;
+        cur = cur->next;
+    }
+    if(cur && prev){
+        prev->next = cur->next;
+        free(cur);
+        neighbors_number--;
+        ret = 1;
+    }else if(prev==NULL && cur!=NULL){
+        neighbors_list = cur->next;
+        free(cur);
+        neighbors_number--;
+        ret =1;
+    }
+    pthread_mutex_unlock(&neighbors_list_mutex);
+    return ret;
+}
+
+struct neighbour* get_neighbour_by_id(int id){
+    struct neighbour*cur;
+    int ret = 0;
+    pthread_mutex_lock(&neighbors_list_mutex);
+    cur = neighbors_list;
+    while(cur){
+        if(cur->id == id){
+            return cur;
+        }
+        cur = cur->next;
+    }
+    return NULL;
+    pthread_mutex_unlock(&neighbors_list_mutex);
+}
+
+
+struct neighbour* add_neighbour(int id,int socket,struct in_addr addr,int port) {
+    struct neighbour* n;   
+    n = get_neighbour_by_id(id);
+    if(n!=NULL) return n;//Già presente!
+    if(socket==-1){//Ricevuto dal DS
+        n = neighbour_init_and_connect(id,addr,port);
+    }else{//Ricevuto dal socket listener
+        n = neighbour_init(id,socket,addr,port);
+    }
+    if(n==NULL) return NULL;
+    //Bisogna inserire la nuova socket nel master e aggiornare la socket più grande.
+    //inserisci qua sotto;
+    neighbors_list_add(n);
+    return n;
+}
+
+void neighbors_list_free(){
+    struct neighbour*cur,*prev;
+    cur = neighbors_list;
+    while(cur){
+        prev = cur;
+        cur = cur->next;
+        free(prev);
+    }
+}
+
+/*
+### DS REQUESTER THREAD ########################
+*/
+struct sockaddr ds_addr;
+void* ds_requester_loop(){
+    
+}
+
+
+/*
+### NEIGHBORS MANAGER THREAD ##################
+*/
+
+void* neighbors_loop(int port){
+    int ret,newfd,listener,addrlen,i,len,fdmax;
+
+}
+
+
+/*
+### GLOBALS INIT E FREE #####################
+*/
+void globals_init(){
+    my_id =-1;
+    neighbors_number = 0;
+    FD_ZERO(&master);
+    FD_ZERO(&reading);
+    neighbors_list = NULL;
+    pthread_mutex_init(&neighbors_list_mutex,NULL);
+    pthread_mutex_init(&fd_mutex,NULL);
+}
+
+void globals_free(){
+    neighbors_list_free();
+}
+/*
+### USER INTERFACE ############################
+*/
+int find_command(char* command){
+    char command_list[][PEER_MAX_COMMAND_SIZE] = PEER_COMMAND_LIST;
+    for(int i = 0; i<PEER_COMMANDS_NUMBER; i++){
+        if(strcmp(command,command_list[i]) == 0 ) return i;
+    }
+    return -1;
+}
+
+void main_loop(){
+
+}
+
+int main(int argc,char*argv[]){
+    int port;
+    if(argc>1){
+        port = atoi(argv[1]);
+    }else{
+        port = DEFAULT_PEER_PORT;
+    }
+
+
+}
+
+
+
+/*
+
+int find_command(char* command){
+    char command_list[][SERVER_MAX_COMMAND_SIZE] = SERVER_COMMAND_LIST;
+    for(int i = 0; i<SERVER_COMMANDS_NUMBER; i++){
+        if(strcmp(command,command_list[i]) == 0 ) return i;
+    }
+    return -1;
+}
+
+//Sveglio il thread in ascolto per farlo uscire dal loop
+void send_exit_packet(int ds_port){
+    char msg[5] = "exit";
+    int socket;
+    socklen_t ds_addrlen;
+    struct sockaddr_in ds_addr,socket_addr;
+    if(open_udp_socket(&socket,&socket_addr,ds_port+1)){
+        perror("Impossibile aprire socket");
+        loop_flag = 0;
+        exit(EXIT_FAILURE);
+    }
+    ds_addr.sin_family = AF_INET; //Tipo di socket
+    ds_addr.sin_port = htons(ds_port);//Porta
+    inet_pton(AF_INET,LOCAL_HOST,&ds_addr.sin_addr);
+    ds_addrlen = sizeof(ds_addr);
+    sendto(socket,msg,5,0,(struct sockaddr*)&ds_addr,ds_addrlen);
+    close(socket);
+}
+
+
+void user_loop(int port){
+    char msg[40];
+    int id;
+    int args_number;
+    int command_index;
+    char args[2][13];
+    printf(SERVER_WELCOME_MSG);
+    while(loop_flag){
+        printf(">> ");
+        fgets(msg, 40, stdin);
+        args_number = sscanf(msg,"%s %s",args[0],args[1]);
+        // arg_len = my_parser(&args,msg);
+        if(args_number>0){
+            command_index = find_command(args[0]);
+        }else{
+            command_index = -1;
+        }
+        switch(command_index){//Gestione dei comandi riconosciuti
+            //__help__
+            case 0:
+                printf(SERVER_HELP_MSG);
+                break;
+            //__showpeers__
+            case 1:
+                peers_list_print();
+                break;
+            //__showneighbor__
+            case 2:
+                if(args_number < 2){
+                    printf("Manca l'ID\n");
+                    break;
+                }
+                id = atoi(args[1]);
+                if(id<0 || id>=peers_number){
+                    printf("ID non riconosciuto!\n");
+                }
+                peers_table_print_peer_neighbor(id);
+            break;
+            //__esc__
+            case 3:
+                loop_flag = 0;
+                printf("Chiusura in corso...\n");
+                send_exit_packet(port);
+                sleep(1);
+            break;
+            //__showpeersinfo__
+            case 4:
+                peers_table_print_all_peers();
+            break;
+            //__comando non riconosciuto__
+            default:
+                printf("Comando non riconosciuto!\n");
+            break;
+        }
+
+    }
+
+
+}
+
+
+int main(int argc, char* argv[]){
+    pthread_t service_thread;
+    int port;
+    void* thread_ret;
+    globals_init();
+    if(argc>1){
+        port = atoi(argv[1]);
+    }else{
+        port = DEFAULT_PORT;
+    }
+    if(pthread_create(&service_thread,NULL,thread_loop,(void*)&port)){
+        perror("Errore nella creazione del thread\n");
+        exit(EXIT_FAILURE);
+    }
+    sleep(1);
+    user_loop(port);
+    pthread_join(service_thread,&thread_ret);
+    globals_free();
+    printf("Ciao, ciao!\n");
+}
+
+*/
